c23fe947745300a7b9ebe61bc70ca8ce81af9bff
misoudon956 <ykuk1613@gmail.com>
2026-04-25 23:51:39 +0900

    レーン改良途中。README書いたよ！！

--- Assets/Scripts/no_tu_move.cs
+++ Assets/Scripts/no_tu_move.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
-public class notu_script : MonoBehaviour
+public class no_tu_move : MonoBehaviour
 {
-    [SerializeField] private Vector3 speed = new Vector3(0, -1, 0);
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +11,6 @@ public class notu_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += speed * Time.deltaTime;
+        
     }
 }
